e553489085d2eaf8b9ad6d900fea8367d2c99855
Natalie <you@example.com>
2022-03-20 09:25:37 -0700

    Added testing utilities and small description fix

--- src/features/tasks/TaskListItem.js
+++ src/features/tasks/TaskListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TaskListItem() {
   return (
     <li className="list-group-item">
-        <p>Show the tasks' text and priority and a delete button as a Bootstrap list group item</p>
+        <p>Show the tasks' text and priority and a checkbox and a delete button as a Bootstrap list group item</p>
         <a href="https://getbootstrap.com/docs/5.1/components/list-group/">Bootstrap Reference</a>
     </li>
   )
---
+++ src/test/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import taskReducer from '../tasks/taskSlice';
+
+/**
+ * Overrides React Testing Library's render method to add wrappers for React Router and Redux
+ * Allows you to specify as an option a preloaded state for the entire test Redux Store
+ *
+ * @param {Element} ui - Passed straight through to React Testing Library's render function
+ * @param {Object} options - Adds preloadedState property for giving Redux a preloaded state
+ * @returns the return value from React Testing Library's render function
+ */
+function customRender(ui, options = {}) {
+  // Test Redux Store with preloaded state
+  const testStore = configureStore({
+    reducer: {
+      tasks: taskReducer,
+    },
+    preloadedState: options.preloadedState,
+  });
+
+  // Wrapping Component with Redux and React Router
+  function Wrappers({ children }) {
+    return (
+      <Provider store={testStore}>
+        <BrowserRouter>
+          { children }
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+
+  // Return the result of calling React Testing Library's render function, but with the wrappers
+  return render(ui, { wrapper: Wrappers, ...options });
+}
+
+// Re-export everything
+export * from '@testing-library/react';
+
+// Override render method
+export { customRender as render };
