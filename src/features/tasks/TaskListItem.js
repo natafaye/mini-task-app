b20a905c764ecfbf991d2ce001a0e5e36ae85304
Natalie <you@example.com>
2022-03-20 13:16:02 -0700

    Added delete button

--- src/features/tasks/TaskListItem.js
+++ src/features/tasks/TaskListItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function TaskListItem() {
+export default function TaskListItem({ task }) {
   return (
     <li className="list-group-item">
         <p>Show the tasks' text and priority and a checkbox and a delete button as a Bootstrap list group item</p>
         <a href="https://getbootstrap.com/docs/5.1/components/list-group/">Bootstrap Reference</a>
+        <button class="btn btn-danger">Delete</button>
     </li>
   )
 }
